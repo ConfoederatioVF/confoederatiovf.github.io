e58c324145dd2a37af6055af5c3909566063665a
Confoederatio <htmlprogrammingbn@gmail.com>
2026-03-12 10:31:40 +0000

    Update gallery_tiles.js

--- js/config/homepage/gallery_tiles/gallery_tiles.js
+++ js/config/homepage/gallery_tiles/gallery_tiles.js
@@ -6,11 +6,12 @@ config.homepage.gallery.tiles = {
     font_position: "centre",
     font_size: 3,
     font_weight: 700,
+    is_base_node: true,
     size: 1,
     x: 0,
     y: 28,
     
-    colour: "midnight-blue"
+    colour: "light-blue"
   },
   
   datasets: {
@@ -19,7 +20,6 @@ config.homepage.gallery.tiles = {
     font_position: "bottom-right",
     font_size: 2,
     font_weight: 500,
-    is_base_node: true,
     size: 1,
     x: 32,
     y: 32,
@@ -27,5 +27,250 @@ config.homepage.gallery.tiles = {
     colour: "light-grey"
   },
   
+  histmap: {
+    name: "Histmap",
+    animation: "histmap",
+    font_position: "top-left",
+    font_size: 2,
+    font_weight: 300,
+    size: 2,
+    x: 64,
+    y: 32,
+    
+    colour: "transparent-sepia"
+  },
   
+  livemap_collation: {
+    name: "Livemap/Collation",
+    animation: "livemap-collation",
+    font_position: "bottom-left",
+    font_size: 2,
+    font_weight: 100,
+    size: 2,
+    x: 32,
+    y: 64,
+    
+    colour: "midnight-blue"
+  },
+  
+  software_and_tooling_crd: {
+    name: "Software & Tooling",
+    animation: "crd-software-and-tooling",
+    font_position: "bottom-right",
+    font_size: 2,
+    font_weight: 700,
+    size: 1,
+    x: 64,
+    y: 64,
+    
+    colour: "copper"
+  },
+  naissance: {
+    name: "Naissance<br>HGIS",
+    animation: "naissance",
+    font_position: "centre",
+    font_size: 2,
+    font_weight: 500,
+    default_bookmark: true,
+    default_pin: true,
+    size: 2,
+    x: 96,
+    y: 68,
+    
+    colour: "bright-yellow"
+  },
+  eoscala: {
+    name: "Eoscala",
+    animation: "eoscala",
+    font_position: "bottom-right",
+    font_size: 1,
+    font_weight: 500,
+    size: 5,
+    x: 92,
+    y: 32,
+    
+    colour: "dark-grey"
+  },
+  atlas: {
+    name: "Atlas",
+    animation: "atlas",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 92,
+    y: 48,
+    
+    colour: "gold"
+  },
+  velkscala: {
+    name: "Velkscala",
+    animation: "velkscala",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 76,
+    y: 16,
+    
+    colour: "salmon"
+  },
+  stadester: {
+    name: "Stadestér",
+    animation: "stadester",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 100,
+    size: 6,
+    x: 64,
+    y: 20,
+    
+    colour: "forest-green"
+  },
+  
+  ctd: {
+    name: "CTD",
+    animation: "ctd",
+    font_position: "centre",
+    font_size: 3,
+    font_weight: 700,
+    size: 1,
+    x: 140,
+    y: 28,
+    
+    colour: "forest-green"
+  },
+  ctd_games: {
+    name: "CTD<br>Games",
+    animation: "ctd-games",
+    font_position: "bottom-left",
+    font_size: 2,
+    font_weight: 900,
+    size: 3,
+    x: 216,
+    y: 28,
+    
+    colour: "blurple"
+  },
+  analytical_engine: {
+    name: "Analytical Engine",
+    animation: "analytical-engine",
+    font_position: "bottom-right",
+    font_size: 1,
+    font_weight: 700,
+    size: 4,
+    x: 240,
+    y: 34,
+    
+    colour: "dark-grey"
+  },
+  triumph_and_tragedy_one: {
+    name: "Triumph &<br>Tragedy I",
+    animation: "triumph-and-tragedy-one",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 500,
+    size: 4,
+    x: 222,
+    y: 52,
+    
+    colour: "mauve"
+  },
+  triumph_and_tragedy_two: {
+    name: "Triumph & Tragedy II",
+    animation: "triumph-and-tragedy-two",
+    font_position: "bottom-right",
+    font_size: 2,
+    font_weight: 700,
+    size: 1,
+    x: 240,
+    y: 52,
+    
+    colour: "salmon"
+  },
+  system_dynamics: {
+    name: "System Dynamics",
+    animation: "system-dynamics",
+    font_position: "bottom-right",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 240,
+    y: 84,
+    
+    colour: "yellow"
+  },
+  into_modernity: {
+    name: "Into Modernity",
+    animation: "into-modernity",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 256,
+    y: 84,
+    
+    colour: "orange"
+  },
+  proxy_cables: {
+    name: "Proxy Cables",
+    animation: "proxy-cables",
+    font_position: "top-left",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 272,
+    y: 68,
+    
+    colour: "blue"
+  },
+  last_man_standing: {
+    name: "Last Man Standing",
+    animation: "last-man-standing",
+    font_position: "bottom-right",
+    font_size: 1,
+    font_weight: 300,
+    size: 5,
+    x: 272,
+    y: 52,
+    
+    colour: "dark-grey"
+  },
+  
+  software_and_tooling_ctd: {
+    name: "Software &<br>Tooling",
+    animation: "software-and-tooling-ctd",
+    font_position: "bottom-left",
+    font_size: 2,
+    font_weight: 700,
+    size: 1,
+    x: 178,
+    y: 28,
+    
+    colour: "dark-blurple"
+  },
+  vercengen: {
+    name: "Vercengen",
+    animation: "vercengen",
+    font_position: "centre",
+    font_size: 2,
+    font_weight: 900,
+    size: 2,
+    x: 178,
+    y: 60,
+    
+    colour: "blurple"
+  },
+  eleven_fiftynine: {
+    name: "11:59",
+    animation: "elevan-fifty-nine",
+    font_position: "centre",
+    font_size: 1,
+    font_weight: 300,
+    size: 6,
+    x: 258,
+    y: 34,
+    
+    colour: "red"
+  }
 };
